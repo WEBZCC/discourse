289bc20305d0855f43da41b8818ce3549df94e2f
OsamaSayegh <asooomaasoooma90@gmail.com>
2022-07-13 09:27:58 +0300

    introduce the abstract component items-list and notifications-list

--- app/assets/javascripts/discourse/app/models/notification.js
+++ app/assets/javascripts/discourse/app/models/notification.js
@@ -4,6 +4,36 @@ import { tracked } from "@glimmer/tracking";
 const DEFAULT_ITEM = "user-menu/notification-item";
 const _componentForType = {};
 
+function defaultComponentForType() {
+  return {
+    bookmark_reminder: "user-menu/bookmark-reminder-notification-item",
+    custom: "user-menu/custom-notification-item",
+    granted_badge: "user-menu/granted-badge-notification-item",
+    group_mentioned: "user-menu/group-mentioned-notification-item",
+    group_message_summary: "user-menu/group-message-summary-notification-item",
+    invitee_accepted: "user-menu/invitee-accepted-notification-item",
+    liked: "user-menu/liked-notification-item",
+    liked_consolidated: "user-menu/liked-consolidated-notification-item",
+    membership_request_accepted:
+      "user-menu/membership-request-accepted-notification-item",
+    membership_request_consolidated:
+      "user-menu/membership-request-consolidated-notification-item",
+    moved_post: "user-menu/moved-post-notification-item",
+    watching_first_post: "user-menu/watching-first-post-notification-item",
+  };
+}
+
+export function registerUserMenuComponentForNotificationType(
+  notificationType,
+  component
+) {
+  _componentForType[notificationType] = component;
+}
+
+export function resetUserMenuCustomComponents() {
+  _componentForType = defaultComponentForType();
+}
+
 export default class Notification extends RestModel {
   @tracked read;
 
